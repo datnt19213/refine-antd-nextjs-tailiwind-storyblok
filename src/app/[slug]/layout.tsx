3c8cf5f0d48fc16a8bdcc4ecc9bf87974f996fae
datnt19213 <datnguyen.frontend.dev@gmail.com>
2025-03-26 11:31:54 +0700

    feat: Use environment variable for Formbricks app URL
    This commit updates the Formbricks initialization to use an environment variable for the `appUrl`. This change allows for easier configuration and deployment across different environments by avoiding hardcoding the Formbricks application URL. The `NEXT_PUBLIC_FORMBRICKS_URL` environment variable is now used to specify the URL.

--- src/app/[slug]/layout.tsx
+++ src/app/[slug]/layout.tsx
@@ -9,7 +9,7 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
             formbricks
                 .init({
                     environmentId: "cm886h70s0009o7016x1zkzei",
-                    appUrl: "https://formbricks-production-935b.up.railway.app/",
+                    appUrl: process.env.NEXT_PUBLIC_FORMBRICKS_URL,
                 })
                 .then(() => console.log("✅ Formbricks initialized"))
                 .catch((err) => console.error("❌ Formbricks init failed", err));
